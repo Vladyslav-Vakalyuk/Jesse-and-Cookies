8f1e86b9cda089b6842b35dbea36d49123d777b1
Vladyslav-Vakalyuk <vlad.vakalyk@gmail.com>
2021-02-18 22:55:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-
+/*
+ * Complete the cookies function below.
+ */
 function cookies($k, $A) {
 $step = 0;
 do{
@@ -22,3 +24,25 @@ function checkIfAllCoockiesIsBiggerThanSomeValue ($value, $cookies){
     }
     return $result;
 }
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%[^\n]", $nk_temp);
+$nk = explode(' ', $nk_temp);
+
+$n = intval($nk[0]);
+
+$k = intval($nk[1]);
+
+fscanf($stdin, "%[^\n]", $A_temp);
+
+$A = array_map('intval', preg_split('/ /', $A_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = cookies($k, $A);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);

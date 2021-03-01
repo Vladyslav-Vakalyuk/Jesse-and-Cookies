b3755c70144621a7b610e0d97551197f9c97aa9d
Vladyslav-Vakalyuk <vlad.vakalyk@gmail.com>
2021-03-01 22:05:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,74 @@
-
 <?php
 
 /*
  * Complete the cookies function below.
  */
-function cookies($k, $A) {
-$step = 0;
-do{
-    $step++;
-    if(count($A) < 2) return -1;
-    $newCookie = 1 * array_shift($A) + 2 * array_shift($A);
-    $A[]=$newCookie;
-    sort($A);
-}while(!checkIfAllCoockiesIsBiggerThanSomeValue ($k, $A));
-return $step;
+
+/**
+ * @param $k
+ * @param $A
+ *
+ * @return int
+ */
+function cookies( $k, $A ) {
+	$heap = new SplMinHeap();
+	foreach ( $A as $value ) {
+		$heap->insert( $value );
+	}
+	$step = 0;
+	do {
+		$step ++;
+        
+		if ( $heap->count() < 2 ) {
+			return - 1;
+		}
+        
+		$firstCookie = $heap->current();
+		$heap->next();
+		$secondCookie = $heap->current();
+		$heap->next();
+		$newCookie = 1 * $firstCookie + 2 * $secondCookie;
+		$heap->insert( $newCookie );
+	} while ( checkIfAllCoockiesIsBiggerThanSomeValue( $k, $heap ) );
+
+	return $step;
 }
-function checkIfAllCoockiesIsBiggerThanSomeValue ($value, $cookies){
-    $result = true;
-    foreach($cookies as $cookie){
-        if($cookie < $value){
-            $result = false;
-        }
-    }
-    return $result;
+
+/**
+ * @param $value
+ * @param $heap
+ *
+ * @return bool
+ */
+function checkIfAllCoockiesIsBiggerThanSomeValue( $value, $heap ) {
+	$result = false;
+	do {
+		if ( $heap->current() < $value ) {
+			$result = true;
+		}
+	} while ( $heap->next() );
+
+	return $result;
 }
 
-$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+$fptr = fopen( getenv( "OUTPUT_PATH" ), "w" );
 
-$stdin = fopen("php://stdin", "r");
+$stdin = fopen( "php://stdin", "r" );
 
-fscanf($stdin, "%[^\n]", $nk_temp);
-$nk = explode(' ', $nk_temp);
+fscanf( $stdin, "%[^\n]", $nk_temp );
+$nk = explode( ' ', $nk_temp );
 
-$n = intval($nk[0]);
+$n = intval( $nk[0] );
 
-$k = intval($nk[1]);
+$k = intval( $nk[1] );
 
-fscanf($stdin, "%[^\n]", $A_temp);
+fscanf( $stdin, "%[^\n]", $A_temp );
 
-$A = array_map('intval', preg_split('/ /', $A_temp, -1, PREG_SPLIT_NO_EMPTY));
+$A = array_map( 'intval', preg_split( '/ /', $A_temp, - 1, PREG_SPLIT_NO_EMPTY ) );
 
-$result = cookies($k, $A);
+$result = cookies( $k, $A );
 
-fwrite($fptr, $result . "\n");
+fwrite( $fptr, $result . "\n" );
 
-fclose($stdin);
-fclose($fptr);
+fclose( $stdin );
+fclose( $fptr );
